d3e707eb519f74af6143b143553b4e712c925f13
ngocloan202 <chauthingocloan0309@gmail.com>
2025-05-23 21:22:18 +0700

    Add frmNewInvoice form and update project structure

--- VitaPharm/Program.cs
+++ VitaPharm/Program.cs
@@ -1,3 +1,4 @@
+using VitaPharm.Forms;
 namespace VitaPharm
 {
     internal static class Program
@@ -11,7 +12,7 @@ namespace VitaPharm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new frmNewInvoice());
         }
     }
 }
