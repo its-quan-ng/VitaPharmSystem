6f368ec79f929c7c254381aa863ccfece2470bc0
Nguyen Quan <ngquan6704@gmail.com>
2025-05-23 21:12:53 +0700

    Refactor code structure for improved readability and maintainability

---
+++ VitaPharm/Forms/frmSignIn.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VitaPharm.Forms
+{
+    partial class frmSignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.vitapharm_transparent;
+            pictureBox1.Location = new Point(206, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(129, 104);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.user;
+            pictureBox2.Location = new Point(55, 145);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(28, 39);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(55, 182);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(440, 2);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(55, 260);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(440, 2);
+            panel2.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources._lock;
+            pictureBox3.Location = new Point(50, 216);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(42, 51);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // frmSignIn
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(548, 380);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox3);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmSignIn";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sign In";
+            Load += frmSignIn_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ VitaPharm/Forms/frmSignIn.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VitaPharm.Forms
+{
+    public partial class frmSignIn : DevExpress.XtraEditors.XtraForm
+    {
+        public frmSignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSignIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
