056102afdfb1233770a9f6a7171f4a631c56dd6a
Nguyen Quan <ngquan6704@gmail.com>
2025-05-23 21:31:39 +0700

    Enhance frmSignIn layout with additional controls and improved styling

--- VitaPharm/Forms/frmSignIn.Designer.cs
+++ VitaPharm/Forms/frmSignIn.Designer.cs
@@ -33,6 +33,12 @@
             panel1 = new Panel();
             panel2 = new Panel();
             pictureBox3 = new PictureBox();
+            checkButton1 = new DevExpress.XtraEditors.CheckButton();
+            labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -51,7 +57,7 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.user;
-            pictureBox2.Location = new Point(55, 145);
+            pictureBox2.Location = new Point(49, 135);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(28, 39);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
@@ -60,14 +66,16 @@
             // 
             // panel1
             // 
-            panel1.Location = new Point(55, 182);
+            panel1.BackColor = Color.FromArgb(61, 189, 236);
+            panel1.Location = new Point(49, 172);
             panel1.Name = "panel1";
             panel1.Size = new Size(440, 2);
             panel1.TabIndex = 2;
             // 
             // panel2
             // 
-            panel2.Location = new Point(55, 260);
+            panel2.BackColor = Color.FromArgb(61, 189, 236);
+            panel2.Location = new Point(49, 250);
             panel2.Name = "panel2";
             panel2.Size = new Size(440, 2);
             panel2.TabIndex = 4;
@@ -75,18 +83,85 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources._lock;
-            pictureBox3.Location = new Point(50, 216);
+            pictureBox3.Location = new Point(44, 206);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(42, 51);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 3;
             pictureBox3.TabStop = false;
             // 
+            // checkButton1
+            // 
+            checkButton1.Location = new Point(337, 275);
+            checkButton1.Name = "checkButton1";
+            checkButton1.Size = new Size(20, 20);
+            checkButton1.TabIndex = 5;
+            // 
+            // labelControl1
+            // 
+            labelControl1.Appearance.Font = new Font("Inter Semi Bold", 9F, FontStyle.Bold | FontStyle.Italic);
+            labelControl1.Appearance.Options.UseFont = true;
+            labelControl1.Location = new Point(363, 277);
+            labelControl1.Name = "labelControl1";
+            labelControl1.Size = new Size(117, 18);
+            labelControl1.TabIndex = 6;
+            labelControl1.Text = "Show password";
+            // 
+            // simpleButton1
+            // 
+            simpleButton1.Appearance.BackColor = Color.FromArgb(16, 53, 113);
+            simpleButton1.Appearance.Font = new Font("Inter", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            simpleButton1.Appearance.Options.UseBackColor = true;
+            simpleButton1.Appearance.Options.UseFont = true;
+            simpleButton1.AppearanceHovered.BackColor = Color.FromArgb(170, 188, 217);
+            simpleButton1.AppearanceHovered.Options.UseBackColor = true;
+            simpleButton1.Location = new Point(49, 314);
+            simpleButton1.Name = "simpleButton1";
+            simpleButton1.Size = new Size(440, 50);
+            simpleButton1.TabIndex = 7;
+            simpleButton1.Text = "Sign In";
+            // 
+            // labelControl2
+            // 
+            labelControl2.Appearance.Font = new Font("Inter", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelControl2.Appearance.ForeColor = Color.FromArgb(66, 176, 109);
+            labelControl2.Appearance.Options.UseFont = true;
+            labelControl2.Appearance.Options.UseForeColor = true;
+            labelControl2.Location = new Point(251, 380);
+            labelControl2.Name = "labelControl2";
+            labelControl2.Size = new Size(36, 24);
+            labelControl2.TabIndex = 8;
+            labelControl2.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(92, 137);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 34);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(92, 215);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(396, 34);
+            textBox2.TabIndex = 10;
+            // 
             // frmSignIn
             // 
+            Appearance.BackColor = SystemColors.Window;
+            Appearance.Options.UseBackColor = true;
             AutoScaleDimensions = new SizeF(7F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(548, 380);
+            ClientSize = new Size(548, 416);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(labelControl2);
+            Controls.Add(simpleButton1);
+            Controls.Add(labelControl1);
+            Controls.Add(checkButton1);
             Controls.Add(panel2);
             Controls.Add(pictureBox3);
             Controls.Add(panel1);
@@ -101,6 +176,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -110,5 +186,11 @@
         private Panel panel1;
         private Panel panel2;
         private PictureBox pictureBox3;
+        private DevExpress.XtraEditors.CheckButton checkButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
