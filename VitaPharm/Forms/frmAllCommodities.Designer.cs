7cddb73ab2fd2c77d7e8fa511d6244ca8a303425
Nguyen Quan <ngquan6704@gmail.com>
2025-05-24 11:02:39 +0700

    Add grid control and view to frmAllCommodities form

--- VitaPharm/Forms/frmAllCommodities.Designer.cs
+++ VitaPharm/Forms/frmAllCommodities.Designer.cs
@@ -28,18 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            gridColID = new DevExpress.XtraGrid.Columns.GridColumn();
+            gridControl1 = new DevExpress.XtraGrid.GridControl();
+            ((System.ComponentModel.ISupportInitialize)gridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gridControl1).BeginInit();
             SuspendLayout();
+            // 
+            // gridView1
+            // 
+            gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] { gridColID });
+            gridView1.GridControl = gridControl1;
+            gridView1.Name = "gridView1";
+            // 
+            // gridColID
+            // 
+            gridColID.Caption = "ID";
+            gridColID.FieldName = "CommodityID";
+            gridColID.MinWidth = 25;
+            gridColID.Name = "gridColID";
+            gridColID.Visible = true;
+            gridColID.VisibleIndex = 0;
+            gridColID.Width = 94;
+            // 
+            // gridControl1
+            // 
+            gridControl1.Dock = DockStyle.Fill;
+            gridControl1.Location = new Point(0, 0);
+            gridControl1.MainView = gridView1;
+            gridControl1.Name = "gridControl1";
+            gridControl1.Size = new Size(831, 462);
+            gridControl1.TabIndex = 0;
+            gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] { gridView1 });
             // 
             // frmAllCommodities
             // 
             AutoScaleDimensions = new SizeF(7F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(831, 462);
+            Controls.Add(gridControl1);
             Name = "frmAllCommodities";
             Text = "frmAllCommodities";
+            ((System.ComponentModel.ISupportInitialize)gridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gridControl1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColID;
+        private DevExpress.XtraGrid.GridControl gridControl1;
     }
 }
