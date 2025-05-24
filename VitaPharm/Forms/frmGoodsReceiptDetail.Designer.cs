2d35e8a7e623c1a5a5ca5315ce5b690b8019b06f
ngocloan202 <chauthingocloan0309@gmail.com>
2025-05-24 21:25:45 +0700

    Add Receipt Code column and update UI elements

--- VitaPharm/Forms/frmGoodsReceiptDetail.Designer.cs
+++ VitaPharm/Forms/frmGoodsReceiptDetail.Designer.cs
@@ -31,6 +31,7 @@
             panelControl1 = new DevExpress.XtraEditors.PanelControl();
             gridControl1 = new DevExpress.XtraGrid.GridControl();
             gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            colReceiptCode = new DevExpress.XtraGrid.Columns.GridColumn();
             colBatchCode = new DevExpress.XtraGrid.Columns.GridColumn();
             colMfgDate = new DevExpress.XtraGrid.Columns.GridColumn();
             colExpDate = new DevExpress.XtraGrid.Columns.GridColumn();
@@ -38,13 +39,13 @@
             colQuantity = new DevExpress.XtraGrid.Columns.GridColumn();
             colAmount = new DevExpress.XtraGrid.Columns.GridColumn();
             panel1 = new Panel();
+            btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            btnReload = new DevExpress.XtraEditors.SimpleButton();
             panel2 = new Panel();
-            labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            lblTotalAmount = new DevExpress.XtraEditors.LabelControl();
             labelControl2 = new DevExpress.XtraEditors.LabelControl();
             lblTotalQuantity = new DevExpress.XtraEditors.LabelControl();
-            lblTotalAmount = new DevExpress.XtraEditors.LabelControl();
-            btnReload = new DevExpress.XtraEditors.SimpleButton();
-            btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            labelControl1 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)panelControl1).BeginInit();
             panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)gridControl1).BeginInit();
@@ -74,9 +75,19 @@
             // 
             // gridView1
             // 
-            gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] { colBatchCode, colMfgDate, colExpDate, colPurchasePrice, colQuantity, colAmount });
+            gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] { colReceiptCode, colBatchCode, colMfgDate, colExpDate, colPurchasePrice, colQuantity, colAmount });
             gridView1.GridControl = gridControl1;
             gridView1.Name = "gridView1";
+            // 
+            // colReceiptCode
+            // 
+            colReceiptCode.Caption = "Receipt Code";
+            colReceiptCode.FieldName = "ReceiptCode";
+            colReceiptCode.MinWidth = 25;
+            colReceiptCode.Name = "colReceiptCode";
+            colReceiptCode.Visible = true;
+            colReceiptCode.VisibleIndex = 0;
+            colReceiptCode.Width = 94;
             // 
             // colBatchCode
             // 
@@ -85,7 +96,7 @@
             colBatchCode.MinWidth = 25;
             colBatchCode.Name = "colBatchCode";
             colBatchCode.Visible = true;
-            colBatchCode.VisibleIndex = 0;
+            colBatchCode.VisibleIndex = 1;
             colBatchCode.Width = 94;
             // 
             // colMfgDate
@@ -95,7 +106,7 @@
             colMfgDate.MinWidth = 25;
             colMfgDate.Name = "colMfgDate";
             colMfgDate.Visible = true;
-            colMfgDate.VisibleIndex = 1;
+            colMfgDate.VisibleIndex = 2;
             colMfgDate.Width = 94;
             // 
             // colExpDate
@@ -105,7 +116,7 @@
             colExpDate.MinWidth = 25;
             colExpDate.Name = "colExpDate";
             colExpDate.Visible = true;
-            colExpDate.VisibleIndex = 2;
+            colExpDate.VisibleIndex = 3;
             colExpDate.Width = 94;
             // 
             // colPurchasePrice
@@ -115,7 +126,7 @@
             colPurchasePrice.MinWidth = 25;
             colPurchasePrice.Name = "colPurchasePrice";
             colPurchasePrice.Visible = true;
-            colPurchasePrice.VisibleIndex = 3;
+            colPurchasePrice.VisibleIndex = 4;
             colPurchasePrice.Width = 94;
             // 
             // colQuantity
@@ -125,7 +136,7 @@
             colQuantity.MinWidth = 25;
             colQuantity.Name = "colQuantity";
             colQuantity.Visible = true;
-            colQuantity.VisibleIndex = 4;
+            colQuantity.VisibleIndex = 5;
             colQuantity.Width = 94;
             // 
             // colAmount
@@ -135,7 +146,7 @@
             colAmount.MinWidth = 25;
             colAmount.Name = "colAmount";
             colAmount.Visible = true;
-            colAmount.VisibleIndex = 5;
+            colAmount.VisibleIndex = 6;
             colAmount.Width = 94;
             // 
             // panel1
@@ -147,6 +158,30 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(621, 63);
             panel1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            btnCancel.Appearance.FontStyleDelta = FontStyle.Bold;
+            btnCancel.Appearance.Options.UseBackColor = true;
+            btnCancel.Appearance.Options.UseFont = true;
+            btnCancel.Location = new Point(498, 19);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(97, 32);
+            btnCancel.TabIndex = 0;
+            btnCancel.Text = "Cancel";
+            // 
+            // btnReload
+            // 
+            btnReload.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
+            btnReload.Appearance.FontStyleDelta = FontStyle.Bold;
+            btnReload.Appearance.Options.UseBackColor = true;
+            btnReload.Appearance.Options.UseFont = true;
+            btnReload.Location = new Point(383, 19);
+            btnReload.Name = "btnReload";
+            btnReload.Size = new Size(97, 32);
+            btnReload.TabIndex = 0;
+            btnReload.Text = "Reload";
             // 
             // panel2
             // 
@@ -160,15 +195,15 @@
             panel2.Size = new Size(621, 88);
             panel2.TabIndex = 2;
             // 
-            // labelControl1
+            // lblTotalAmount
             // 
-            labelControl1.Appearance.Font = new Font("Tahoma", 7.8F, FontStyle.Bold);
-            labelControl1.Appearance.Options.UseFont = true;
-            labelControl1.Location = new Point(27, 15);
-            labelControl1.Name = "labelControl1";
-            labelControl1.Size = new Size(97, 16);
-            labelControl1.TabIndex = 0;
-            labelControl1.Text = "Total Quantity:";
+            lblTotalAmount.Appearance.Font = new Font("Tahoma", 7.8F, FontStyle.Bold);
+            lblTotalAmount.Appearance.Options.UseFont = true;
+            lblTotalAmount.Location = new Point(567, 47);
+            lblTotalAmount.Name = "lblTotalAmount";
+            lblTotalAmount.Size = new Size(28, 16);
+            lblTotalAmount.TabIndex = 0;
+            lblTotalAmount.Text = "0.00";
             // 
             // labelControl2
             // 
@@ -190,39 +225,15 @@
             lblTotalQuantity.TabIndex = 0;
             lblTotalQuantity.Text = "0.00";
             // 
-            // lblTotalAmount
+            // labelControl1
             // 
-            lblTotalAmount.Appearance.Font = new Font("Tahoma", 7.8F, FontStyle.Bold);
-            lblTotalAmount.Appearance.Options.UseFont = true;
-            lblTotalAmount.Location = new Point(567, 47);
-            lblTotalAmount.Name = "lblTotalAmount";
-            lblTotalAmount.Size = new Size(28, 16);
-            lblTotalAmount.TabIndex = 0;
-            lblTotalAmount.Text = "0.00";
-            // 
-            // btnReload
-            // 
-            btnReload.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Warning;
-            btnReload.Appearance.FontStyleDelta = FontStyle.Bold;
-            btnReload.Appearance.Options.UseBackColor = true;
-            btnReload.Appearance.Options.UseFont = true;
-            btnReload.Location = new Point(383, 19);
-            btnReload.Name = "btnReload";
-            btnReload.Size = new Size(97, 32);
-            btnReload.TabIndex = 0;
-            btnReload.Text = "Reload";
-            // 
-            // btnCancel
-            // 
-            btnCancel.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
-            btnCancel.Appearance.FontStyleDelta = FontStyle.Bold;
-            btnCancel.Appearance.Options.UseBackColor = true;
-            btnCancel.Appearance.Options.UseFont = true;
-            btnCancel.Location = new Point(498, 19);
-            btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(97, 32);
-            btnCancel.TabIndex = 0;
-            btnCancel.Text = "Cancel";
+            labelControl1.Appearance.Font = new Font("Tahoma", 7.8F, FontStyle.Bold);
+            labelControl1.Appearance.Options.UseFont = true;
+            labelControl1.Location = new Point(27, 15);
+            labelControl1.Name = "labelControl1";
+            labelControl1.Size = new Size(97, 16);
+            labelControl1.TabIndex = 0;
+            labelControl1.Text = "Total Quantity:";
             // 
             // frmGoodsReceiptDetail
             // 
@@ -263,5 +274,6 @@
         private DevExpress.XtraEditors.LabelControl lblTotalQuantity;
         private DevExpress.XtraEditors.SimpleButton btnReload;
         private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraGrid.Columns.GridColumn colReceiptCode;
     }
 }
