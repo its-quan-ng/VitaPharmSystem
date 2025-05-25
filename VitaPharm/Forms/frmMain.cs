c7afcc3836936e95d4cb3dc9ad8110fd1e54c86d
ngocloan202 <chauthingocloan0309@gmail.com>
2025-05-25 21:25:47 +0700

    Add new user button and update application entry point

--- VitaPharm/Forms/frmMain.cs
+++ VitaPharm/Forms/frmMain.cs
@@ -9,6 +9,7 @@ namespace VitaPharm.Forms
         private readonly Account currentAccount;
         private readonly string currentRole;
         private frmProfile? profileForm = null;
+        frmNewUser? newUserForm = null;
 
         internal frmMain(Account account)
         {
@@ -16,36 +17,61 @@ namespace VitaPharm.Forms
             this.currentAccount = account;
             this.currentRole = account.UserRole ?? "";
             this.IsMdiContainer = true;
-
             ConfigureBasedOnRole();
             OpenProfile();
         }
 
-        #region Handle Sign In
+        #region ConfigureBasedOnRole
         private void ConfigureBasedOnRole()
         {
             bool isAdmin = currentRole.Equals("Admin", StringComparison.OrdinalIgnoreCase);
             bool isUser = currentRole.Equals("User", StringComparison.OrdinalIgnoreCase);
 
             btnProfile.Enabled = true;
-            
+            btnNewUser.Enabled = isAdmin;
         }
 
         private void OpenProfile()
         {
             foreach (var f in this.MdiChildren) f.Close();
-
             var profileForm = new frmProfile(currentAccount)
             {
                 MdiParent = this
             };
             profileForm.Show();
         }
+
+        private void OpenNewUser()
+        {
+            if (!currentRole.Equals("Admin", StringComparison.OrdinalIgnoreCase))
+            {
+                XtraMessageBox.Show("You do not have permission to access this feature!","Notification",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (var f in this.MdiChildren) f.Close();
+
+            if (newUserForm == null || newUserForm.IsDisposed)
+            {
+                newUserForm = new frmNewUser()
+                {
+                    MdiParent = this
+                };
+            }
+            newUserForm.Show();
+            newUserForm.BringToFront();
+        }
         #endregion
 
         private void btnProfile_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
             OpenProfile();
+        }
+
+        private void btnNewUser_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            OpenNewUser();
         }
     }
 }
