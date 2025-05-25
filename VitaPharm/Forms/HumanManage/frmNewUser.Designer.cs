8ba17777d0c09d29a70ac984b3a31a569c3dc6a0
Nguyen Quan <ngquan6704@gmail.com>
2025-05-25 20:00:01 +0700

    Update frmNewUser form layout and replace password fields with TextBox controls

--- VitaPharm/Forms/HumanManage/frmNewUser.Designer.cs
+++ VitaPharm/Forms/HumanManage/frmNewUser.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNewUser));
             chkIsActive = new DevExpress.XtraEditors.CheckEdit();
             dtpBirthday = new DevExpress.XtraEditors.DateEdit();
             chkFemale = new DevExpress.XtraEditors.CheckEdit();
@@ -42,14 +43,14 @@
             labelControl1 = new DevExpress.XtraEditors.LabelControl();
             txtUsername = new DevExpress.XtraEditors.TextEdit();
             panelControl1 = new DevExpress.XtraEditors.PanelControl();
-            txtConfirmPassword = new DevExpress.XtraEditors.TextEdit();
             txtAddress = new DevExpress.XtraEditors.TextEdit();
             labelControl7 = new DevExpress.XtraEditors.LabelControl();
-            txtPassword = new DevExpress.XtraEditors.TextEdit();
             labelControl6 = new DevExpress.XtraEditors.LabelControl();
             panelControl2 = new DevExpress.XtraEditors.PanelControl();
             btnCancel = new DevExpress.XtraEditors.SimpleButton();
             btnAdd = new DevExpress.XtraEditors.SimpleButton();
+            txtPassword = new TextBox();
+            txtConfirmPassword = new TextBox();
             ((System.ComponentModel.ISupportInitialize)chkIsActive.Properties).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dtpBirthday.Properties).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dtpBirthday.Properties.CalendarTimeProperties).BeginInit();
@@ -60,16 +61,14 @@
             ((System.ComponentModel.ISupportInitialize)txtUsername.Properties).BeginInit();
             ((System.ComponentModel.ISupportInitialize)panelControl1).BeginInit();
             panelControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)txtConfirmPassword.Properties).BeginInit();
             ((System.ComponentModel.ISupportInitialize)txtAddress.Properties).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)txtPassword.Properties).BeginInit();
             ((System.ComponentModel.ISupportInitialize)panelControl2).BeginInit();
             panelControl2.SuspendLayout();
             SuspendLayout();
             // 
             // chkIsActive
             // 
-            chkIsActive.Location = new Point(345, 92);
+            chkIsActive.Location = new Point(374, 117);
             chkIsActive.Name = "chkIsActive";
             chkIsActive.Properties.Appearance.BackColor = Color.YellowGreen;
             chkIsActive.Properties.Appearance.Options.UseBackColor = true;
@@ -80,16 +79,16 @@
             // dtpBirthday
             // 
             dtpBirthday.EditValue = null;
-            dtpBirthday.Location = new Point(140, 149);
+            dtpBirthday.Location = new Point(174, 189);
             dtpBirthday.Name = "dtpBirthday";
             dtpBirthday.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] { new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo) });
             dtpBirthday.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] { new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo) });
-            dtpBirthday.Size = new Size(155, 22);
+            dtpBirthday.Size = new Size(150, 22);
             dtpBirthday.TabIndex = 7;
             // 
             // chkFemale
             // 
-            chkFemale.Location = new Point(345, 148);
+            chkFemale.Location = new Point(374, 188);
             chkFemale.Name = "chkFemale";
             chkFemale.Properties.Caption = "Female";
             chkFemale.Size = new Size(81, 24);
@@ -97,90 +96,108 @@
             // 
             // labelControl5
             // 
-            labelControl5.Location = new Point(25, 155);
+            labelControl5.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl5.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl5.ImageOptions.SvgImage");
+            labelControl5.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl5.Location = new Point(22, 152);
             labelControl5.Name = "labelControl5";
-            labelControl5.Size = new Size(51, 16);
+            labelControl5.Size = new Size(77, 24);
             labelControl5.TabIndex = 24;
             labelControl5.Text = "Birthday:";
             // 
             // txtName
             // 
-            txtName.Location = new Point(140, 121);
+            txtName.Location = new Point(174, 154);
             txtName.Name = "txtName";
-            txtName.Size = new Size(286, 22);
+            txtName.Size = new Size(281, 22);
             txtName.TabIndex = 6;
             // 
             // cboRole
             // 
             cboRole.EditValue = "";
-            cboRole.Location = new Point(140, 93);
+            cboRole.Location = new Point(174, 119);
             cboRole.Name = "cboRole";
             cboRole.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] { new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo) });
-            cboRole.Size = new Size(71, 22);
+            cboRole.Size = new Size(66, 22);
             cboRole.TabIndex = 4;
             // 
             // labelControl8
             // 
-            labelControl8.Location = new Point(25, 99);
+            labelControl8.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl8.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl8.ImageOptions.SvgImage");
+            labelControl8.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl8.Location = new Point(22, 116);
             labelControl8.Name = "labelControl8";
-            labelControl8.Size = new Size(30, 16);
+            labelControl8.Size = new Size(56, 24);
             labelControl8.TabIndex = 25;
             labelControl8.Text = "Role:";
             // 
             // labelControl4
             // 
-            labelControl4.Location = new Point(24, 211);
+            labelControl4.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl4.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl4.ImageOptions.SvgImage");
+            labelControl4.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl4.Location = new Point(22, 261);
             labelControl4.Name = "labelControl4";
-            labelControl4.Size = new Size(51, 16);
+            labelControl4.Size = new Size(77, 24);
             labelControl4.TabIndex = 23;
             labelControl4.Text = "Address:";
             // 
             // labelControl3
             // 
-            labelControl3.Location = new Point(25, 183);
+            labelControl3.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl3.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl3.ImageOptions.SvgImage");
+            labelControl3.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl3.Location = new Point(22, 224);
             labelControl3.Name = "labelControl3";
-            labelControl3.Size = new Size(52, 16);
+            labelControl3.Size = new Size(78, 24);
             labelControl3.TabIndex = 22;
             labelControl3.Text = "Contact: ";
             // 
             // txtContact
             // 
-            txtContact.Location = new Point(140, 177);
+            txtContact.Location = new Point(174, 224);
             txtContact.Name = "txtContact";
-            txtContact.Size = new Size(159, 22);
+            txtContact.Size = new Size(154, 22);
             txtContact.TabIndex = 9;
             // 
             // labelControl2
             // 
-            labelControl2.Location = new Point(25, 127);
+            labelControl2.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl2.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl2.ImageOptions.SvgImage");
+            labelControl2.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl2.Location = new Point(22, 188);
             labelControl2.Name = "labelControl2";
-            labelControl2.Size = new Size(62, 16);
+            labelControl2.Size = new Size(88, 24);
             labelControl2.TabIndex = 20;
             labelControl2.Text = "Full Name:";
             // 
             // labelControl1
             // 
-            labelControl1.Location = new Point(25, 15);
+            labelControl1.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl1.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl1.ImageOptions.SvgImage");
+            labelControl1.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl1.Location = new Point(22, 8);
             labelControl1.Name = "labelControl1";
-            labelControl1.Size = new Size(63, 16);
+            labelControl1.Size = new Size(89, 24);
             labelControl1.TabIndex = 0;
             labelControl1.Text = "Username:";
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(140, 9);
+            txtUsername.Location = new Point(174, 12);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(286, 22);
+            txtUsername.Size = new Size(281, 22);
             txtUsername.TabIndex = 1;
             // 
             // panelControl1
             // 
             panelControl1.Controls.Add(txtConfirmPassword);
+            panelControl1.Controls.Add(txtPassword);
             panelControl1.Controls.Add(txtAddress);
             panelControl1.Controls.Add(dtpBirthday);
             panelControl1.Controls.Add(labelControl7);
             panelControl1.Controls.Add(labelControl5);
-            panelControl1.Controls.Add(txtPassword);
             panelControl1.Controls.Add(labelControl4);
             panelControl1.Controls.Add(labelControl6);
             panelControl1.Controls.Add(labelControl1);
@@ -196,43 +213,35 @@
             panelControl1.Dock = DockStyle.Fill;
             panelControl1.Location = new Point(0, 0);
             panelControl1.Name = "panelControl1";
-            panelControl1.Size = new Size(441, 319);
+            panelControl1.Size = new Size(519, 372);
             panelControl1.TabIndex = 33;
-            // 
-            // txtConfirmPassword
-            // 
-            txtConfirmPassword.Location = new Point(140, 65);
-            txtConfirmPassword.Name = "txtConfirmPassword";
-            txtConfirmPassword.Size = new Size(149, 22);
-            txtConfirmPassword.TabIndex = 3;
             // 
             // txtAddress
             // 
-            txtAddress.Location = new Point(140, 205);
+            txtAddress.Location = new Point(174, 263);
             txtAddress.Name = "txtAddress";
-            txtAddress.Size = new Size(286, 22);
+            txtAddress.Size = new Size(281, 22);
             txtAddress.TabIndex = 10;
             // 
             // labelControl7
             // 
-            labelControl7.Location = new Point(25, 71);
+            labelControl7.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl7.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl7.ImageOptions.SvgImage");
+            labelControl7.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl7.Location = new Point(22, 80);
             labelControl7.Name = "labelControl7";
-            labelControl7.Size = new Size(109, 16);
+            labelControl7.Size = new Size(135, 24);
             labelControl7.TabIndex = 35;
             labelControl7.Text = "Confirm Password:";
             // 
-            // txtPassword
-            // 
-            txtPassword.Location = new Point(140, 37);
-            txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(149, 22);
-            txtPassword.TabIndex = 2;
-            // 
             // labelControl6
             // 
-            labelControl6.Location = new Point(25, 43);
+            labelControl6.ImageAlignToText = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            labelControl6.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("labelControl6.ImageOptions.SvgImage");
+            labelControl6.ImageOptions.SvgImageSize = new Size(16, 16);
+            labelControl6.Location = new Point(22, 44);
             labelControl6.Name = "labelControl6";
-            labelControl6.Size = new Size(60, 16);
+            labelControl6.Size = new Size(86, 24);
             labelControl6.TabIndex = 33;
             labelControl6.Text = "Password:";
             // 
@@ -241,9 +250,9 @@
             panelControl2.Controls.Add(btnCancel);
             panelControl2.Controls.Add(btnAdd);
             panelControl2.Dock = DockStyle.Bottom;
-            panelControl2.Location = new Point(0, 255);
+            panelControl2.Location = new Point(0, 308);
             panelControl2.Name = "panelControl2";
-            panelControl2.Size = new Size(441, 64);
+            panelControl2.Size = new Size(519, 64);
             panelControl2.TabIndex = 1;
             // 
             // btnCancel
@@ -255,8 +264,9 @@
             btnCancel.Location = new Point(226, 18);
             btnCancel.Name = "btnCancel";
             btnCancel.Size = new Size(84, 28);
-            btnCancel.TabIndex = 1;
+            btnCancel.TabIndex = 2;
             btnCancel.Text = "Cancel";
+            btnCancel.Click += btnCancel_Click;
             // 
             // btnAdd
             // 
@@ -267,20 +277,40 @@
             btnAdd.Location = new Point(130, 18);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(81, 28);
-            btnAdd.TabIndex = 0;
+            btnAdd.TabIndex = 1;
             btnAdd.Text = "Add";
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.White;
+            txtPassword.BorderStyle = BorderStyle.FixedSingle;
+            txtPassword.Location = new Point(174, 47);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(144, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.BackColor = Color.White;
+            txtConfirmPassword.BorderStyle = BorderStyle.FixedSingle;
+            txtConfirmPassword.Location = new Point(174, 83);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.Size = new Size(144, 23);
+            txtConfirmPassword.TabIndex = 3;
             // 
             // frmNewUser
             // 
             AutoScaleDimensions = new SizeF(7F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(441, 319);
+            ClientSize = new Size(519, 372);
             Controls.Add(panelControl2);
             Controls.Add(panelControl1);
             Name = "frmNewUser";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "New User";
             Load += frmNewUser_Load;
+            Click += frmNewUser_Load;
             ((System.ComponentModel.ISupportInitialize)chkIsActive.Properties).EndInit();
             ((System.ComponentModel.ISupportInitialize)dtpBirthday.Properties.CalendarTimeProperties).EndInit();
             ((System.ComponentModel.ISupportInitialize)dtpBirthday.Properties).EndInit();
@@ -292,9 +322,7 @@
             ((System.ComponentModel.ISupportInitialize)panelControl1).EndInit();
             panelControl1.ResumeLayout(false);
             panelControl1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)txtConfirmPassword.Properties).EndInit();
             ((System.ComponentModel.ISupportInitialize)txtAddress.Properties).EndInit();
-            ((System.ComponentModel.ISupportInitialize)txtPassword.Properties).EndInit();
             ((System.ComponentModel.ISupportInitialize)panelControl2).EndInit();
             panelControl2.ResumeLayout(false);
             ResumeLayout(false);
@@ -317,12 +345,12 @@
         private DevExpress.XtraEditors.TextEdit txtUsername;
         private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl6;
-        private DevExpress.XtraEditors.TextEdit txtConfirmPassword;
         private DevExpress.XtraEditors.LabelControl labelControl7;
-        private DevExpress.XtraEditors.TextEdit txtPassword;
         private DevExpress.XtraEditors.PanelControl panelControl2;
         private DevExpress.XtraEditors.SimpleButton btnCancel;
         private DevExpress.XtraEditors.SimpleButton btnAdd;
         private DevExpress.XtraEditors.TextEdit txtAddress;
+        private TextBox txtConfirmPassword;
+        private TextBox txtPassword;
     }
 }
