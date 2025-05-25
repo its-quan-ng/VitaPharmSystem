718c7135811d2703cad160b19b783abf930fff8b
ngocloan202 <chauthingocloan0309@gmail.com>
2025-05-25 19:21:38 +0700

    fix bug when edit profile doesn't reload data after edit data

--- VitaPharm/Forms/HumanManage/frmProfile.cs
+++ VitaPharm/Forms/HumanManage/frmProfile.cs
@@ -1,4 +1,7 @@
-﻿using DevExpress.XtraEditors;
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
 using Microsoft.EntityFrameworkCore;
 using VitaPharm.Data;
 
@@ -6,67 +9,100 @@ namespace VitaPharm.Forms.HumanManage
 {
     public partial class frmProfile : XtraForm
     {
-        private readonly Account account;
+        private PharmacyDbContext contextAccount;
+        private BindingSource bsAccount;
+        private BindingSource bsEmployee;
+        private readonly int accountId;
+
         internal frmProfile(Account account)
         {
             InitializeComponent();
-            this.account = account;
+            accountId = account.AccountID;
         }
 
         private void frmProfile_Load(object sender, EventArgs e)
         {
-            LoadProfileData();
+            LoadProfileData();    
             ToggleControl(false);
         }
 
         private void LoadProfileData()
         {
-            txtEmployeeID.Text = account.Employee.EmployeeID.ToString();
-            txtUsername.Text = account.Username;
-            txtFullName.Text = account.Employee.EmployeeName;
-            ckcFemale.Checked = account.Employee.Sex == "F";
-            dateBirthday.DateTime = account.Employee.Birthday;
-            txtContact.Text = account.Employee.Contact;
-            txtAddress.Text = account.Employee.EmployeeAddress;
+            contextAccount?.Dispose();
+            contextAccount = new PharmacyDbContext();
+
+            var acct = contextAccount.Accounts
+                        .Include(a => a.Employee)
+                        .First(a => a.AccountID == accountId);
+
+            bsAccount = new BindingSource { DataSource = acct };
+            bsEmployee = new BindingSource { DataSource = acct.Employee };
+
+            txtEmployeeID.DataBindings.Clear();
+            txtEmployeeID.DataBindings.Add(
+                "Text", bsEmployee, nameof(acct.Employee.EmployeeID),
+                true, DataSourceUpdateMode.Never);
+
+            txtUsername.DataBindings.Clear();
+            txtUsername.DataBindings.Add(
+                "Text", bsAccount, nameof(acct.Username),
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+            txtFullName.DataBindings.Clear();
+            txtFullName.DataBindings.Add(
+                "Text", bsEmployee, nameof(acct.Employee.EmployeeName),
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+            ckcFemale.DataBindings.Clear();
+            var sexBinding = new Binding("Checked", bsEmployee, nameof(acct.Employee.Sex),
+                                         true, DataSourceUpdateMode.OnPropertyChanged);
+            sexBinding.Format += (s, e) => e.Value = (e.Value as string) == "F";
+            sexBinding.Parse += (s, e) => e.Value = ((bool)e.Value) ? "F" : "M";
+            ckcFemale.DataBindings.Add(sexBinding);
+
+            dateBirthday.DataBindings.Clear();
+            dateBirthday.DataBindings.Add(
+                "EditValue", bsEmployee, nameof(acct.Employee.Birthday),
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+            txtContact.DataBindings.Clear();
+            txtContact.DataBindings.Add(
+                "Text", bsEmployee, nameof(acct.Employee.Contact),
+                true, DataSourceUpdateMode.OnPropertyChanged);
+
+            txtAddress.DataBindings.Clear();
+            txtAddress.DataBindings.Add(
+                "Text", bsEmployee, nameof(acct.Employee.EmployeeAddress),
+                true, DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        private void ToggleControl(bool enable)
+        private void ToggleControl(bool editing)
         {
             txtEmployeeID.Enabled = false;
 
-            txtUsername.Enabled = enable;
-            txtFullName.Enabled = enable;
-            ckcFemale.Enabled = enable;
-            dateBirthday.Enabled = enable;
-            txtContact.Enabled = enable;
-            txtAddress.Enabled = enable;
+            txtUsername.Enabled = editing;
+            txtFullName.Enabled = editing;
+            ckcFemale.Enabled = editing;
+            dateBirthday.Enabled = editing;
+            txtContact.Enabled = editing;
+            txtAddress.Enabled = editing;
 
-            btnEdit.Enabled = !enable;
-            btnSave.Enabled = enable;
-            btnCancel.Enabled = enable;
+            btnEdit.Enabled = !editing;
+            btnSave.Enabled = editing;
+            btnCancel.Enabled = editing;
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            ToggleControl(true);
+            ToggleControl(true);      
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            using var ctx = new PharmacyDbContext();  
-            var acct = ctx.Accounts
-                         .Include(a => a.Employee)
-                         .FirstOrDefault(a => a.AccountID == account.AccountID);
-            if (acct == null) return;
+            bsAccount.EndEdit();
+            bsEmployee.EndEdit();
 
-            acct.Username = txtUsername.Text.Trim();
-            acct.Employee.EmployeeName = txtFullName.Text.Trim();
-            acct.Employee.Sex = ckcFemale.Checked ? "F" : "M";
-            acct.Employee.Birthday = dateBirthday.DateTime;
-            acct.Employee.Contact = txtContact.Text.Trim();
-            acct.Employee.EmployeeAddress = txtAddress.Text.Trim();
-
-            ctx.SaveChanges();
+            contextAccount.SaveChanges();
 
             XtraMessageBox.Show(
                 "Profile updated successfully.",
@@ -86,9 +122,19 @@ namespace VitaPharm.Forms.HumanManage
                 MessageBoxButtons.YesNo,
                 MessageBoxIcon.Question) == DialogResult.Yes)
             {
-                LoadProfileData();
+                contextAccount.Entry(bsAccount.Current).Reload();
+                contextAccount.Entry(bsEmployee.Current).Reload();
+                bsAccount.ResetBindings(false);
+                bsEmployee.ResetBindings(false);
+
                 ToggleControl(false);
             }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            contextAccount.Dispose();
         }
     }
 }
