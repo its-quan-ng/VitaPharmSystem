e333847c4ed77741ac358a356b7df139144709f5
Quan Nguyen <109069011+its-quan-ng@users.noreply.github.com>
2025-05-26 15:51:49 +0700

    Update Program.cs

--- VitaPharm/Program.cs
+++ VitaPharm/Program.cs
@@ -13,7 +13,7 @@ namespace VitaPharm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmNewBatch());
+            Application.Run(new frmSignIn());
         }
     }
 }
