a68d45fe0569ffdb06524149d880e6db3f73dad2
Nguyen Quan <ngquan6704@gmail.com>
2025-06-02 07:09:09 +0700

    Add rptInvoiceDetail report class and designer files for invoice detail reporting

---
+++ VitaPharm/Reports/rptInvoiceDetail.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace VitaPharm.Reports
+{
+    public partial class rptInvoiceDetail : DevExpress.XtraReports.UI.XtraReport
+    {
+        public rptInvoiceDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
